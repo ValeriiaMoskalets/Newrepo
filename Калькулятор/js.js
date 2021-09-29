ff9fc4718f3f5e23393cafcfb2c7684b61096d2b
ValeriiaMoskalets <89974339+ValeriiaMoskalets@users.noreply.github.com>
2021-09-29 15:28:29 +0300

    Add files via upload

---
+++ Калькулятор/js.js
@@ -0,0 +1,8 @@
+send.addEventListener("click", fnc);
+function fnc(){
+    let val_P1 = P1.value;
+    let val_P0 = P0.value;
+    let val_r = 0;
+	val_r = (parseInt(val_P1) - parseInt(val_P0) / parseInt(val.P0))*100;
+    r.value = val_r;
+}
